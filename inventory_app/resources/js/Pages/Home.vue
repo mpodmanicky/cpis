45a620ca174c318e880e5561d08eca383f611a95
Martin Podmanicky <mpodmanicky34@gmail.com>
2025-02-10 20:50:30 +0100

    created_pp_added_models_migrated

---
+++ inventory_app/resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div class="container text-center">
+        <h1>Inventory</h1>
+    </div>
+
+    <div class="container text-center">
+        <div class="btn-group">
+            <button class="btn btn-primary">Cars</button>
+            <button class="btn btn-primary">Parts</button>
+        </div>
+    </div>
+
+    <Form />
+</template>
